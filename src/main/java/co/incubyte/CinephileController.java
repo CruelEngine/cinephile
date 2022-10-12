7b7e5e76636a7635dd21ac325a4bb48f58df2c68
Abhineeth <abhineeth@incubyte.co>
2022-10-12 11:55:25 +0530

    Hello world Controller Added

---
+++ src/main/java/co/incubyte/CinephileController.java
@@ -0,0 +1,10 @@
+package co.incubyte;
+import io.micronaut.http.annotation.*;
+@Controller("/cinephile")
+public class CinephileController {
+
+    @Get(uri="/", produces="text/plain")
+    public String index() {
+        return "Hello World!";
+    }
+}
